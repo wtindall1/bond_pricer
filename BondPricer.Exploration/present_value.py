71fe196eda4269bf7d22e5b2967c4d2f1ee247b7
wtindall1 <wtindall@hotmail.com>
2023-05-31 17:54:13 +0100

    clean and dirty price

--- BondPricer.Exploration/present_value.py
+++ BondPricer.Exploration/present_value.py
@@ -1,7 +1,8 @@
 import datetime as dt
-from typing import Optional
+from typing import Optional, Dict
 import numpy as np
 import math
+from dateutil.relativedelta import relativedelta
 
 def bond_present_value(
         face_value: float,
@@ -10,33 +11,52 @@ def bond_present_value(
         maturity_date: dt.date,
         risk_free_rate: Optional[float] = None
 
-) -> float:
+) -> Dict[str, float]:
     
     #risk free rate - use US treasury rate if none specified
     if not risk_free_rate:
         risk_free_rate = 0.08
 
-    #calculate number of payments
-    date_diff = (maturity_date - dt.date.today()).days #gets days attribute of timedelta object
-    year_date_diff = date_diff / 365
-    num_payments = math.floor(year_date_diff * coupon_frequency)
-
+    #coupon payment dates
+    coupon_dates = []
+    current_date = maturity_date
+    today = dt.date.today()
+    while current_date > today:
+        coupon_dates.append(current_date)
+        current_date -= relativedelta(months=12 // coupon_frequency)
+    #get previous coupon date for accrued interest calc
+    prev_coupon_date = current_date - relativedelta(months=12 // coupon_frequency)
     
+    coupon_dates.reverse()
+    num_payments = len(coupon_dates)
     coupon = face_value * interest_rate
 
     #calculate PV
     coupons_pv = coupon * ((1 - pow((1+risk_free_rate), - num_payments)) / risk_free_rate)
     maturity_pv = face_value * pow((1+risk_free_rate), - num_payments)
-    present_value = coupons_pv + maturity_pv
+    dirty_price = coupons_pv + maturity_pv
 
-    return present_value
+    #subtract accrued interest to get clean price
+    num_days_accrued = (today - prev_coupon_date).days
+    days_in_coupon_period = (coupon_dates[0] - prev_coupon_date).days
+    accrued_interest = coupon * (num_days_accrued / days_in_coupon_period)
+
+    clean_price = dirty_price - accrued_interest
+
+
+    return {
+        "CleanPrice": clean_price,
+        "DirtyPrice": dirty_price
+    }
 
 
     
 
 
     #should we be using YTM or risk free rate?? rfr to reflect market dynamics more, ytm for long term investment value
-    #ASSUMES PAYMENT JUST BEEN MADE - Add adjustments for dates / dirty vs clean price
 
 
-print(bond_present_value(100, 0.1, 4, dt.date(2027, 1, 1)))
+print(bond_present_value(100, 0.1, 4, dt.date(2023, 6, 30)))
+
+
+
