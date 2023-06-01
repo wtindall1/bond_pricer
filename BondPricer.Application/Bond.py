209e13c1db87dff28f45249ea8b02c8d95e38beb
wtindall1 <wtindall@hotmail.com>
2023-06-01 22:27:08 +0100

    bond class

--- BondPricer.Application/Bond.py
+++ BondPricer.Application/Bond.py
@@ -1,5 +1,6 @@
 import datetime as dt
 from typing import Optional, Dict
+from dateutil.relativedelta import relativedelta
 
 class Bond:
 
@@ -15,12 +16,35 @@ class Bond:
         self.coupon_frequency = coupon_frequency
         self.maturity_date = maturity_date
 
-    def treasury_yield_closest_maturity(self):
+    def get_coupon_dates(self) -> None:
+        self.coupon_dates = []
+        date = self.maturity_date
+        #work back from maturity date to find all future coupon dates (estimated)
+        while date > dt.date.today():
+            self.coupon_dates.append(date)
+            date -= relativedelta(months=12 // self.coupon_frequency)
+        
+
+    def closest_maturity_UST(self):
+        #find the yield on the US treasury with closest maturity to the bond
+        maturities = {
+            0.25: "3month",
+            2: "2year",
+            5: "5year",
+            7: "7year",
+            10: "10year",
+            30: "30year"
+        }
+        years_to_maturity = (self.maturity_date - dt.date.today()).days / 365
+        closest_maturity = min(maturities.keys(), key=lambda x: abs(years_to_maturity - x))
+        return maturities[closest_maturity]
+
+
+    def closest_maturity_UST_yield(self):
         ...
 
-    def get_coupon_dates(self):
-        #last is maturity
-        ...
+
+    
 
     def accrued_interest(self):
         ...
