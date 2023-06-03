015b1bc3b68f58cc42e2cb0a748503715e6c5c5b
wtindall1 <wtindall@hotmail.com>
2023-06-03 22:07:33 +0100

    adjusted calc to handle zero coupon bonds

--- API/Application/ValuationPV.py
+++ API/Application/ValuationPV.py
@@ -13,17 +13,27 @@ class ValuationPV:
 
         #inputs
         coupon_dates = self.bond.get_coupon_dates()
-        number_periods = len(coupon_dates)
+        #for zero coupon bond, set num_periods to number of years
+        if self.bond.coupon_frequency == 0:
+            number_periods = (coupon_dates[-1] - dt.date.today()).days / 365
+        else:
+            number_periods = len(coupon_dates)
+        
         discount_rate = self.bond.get_proxy_yield() if not discount_rate_specified else discount_rate_specified
         
         #exit if discount rate not specified and no proxy found
         if discount_rate is None:
             print("No discount rate was specified and no proxy could be found.")
             return None
+        
+        #adjust discount rate for coupon frequency
+        if self.bond.coupon_frequency > 1:
+            discount_rate = (1+discount_rate)**(1/self.bond.coupon_frequency) - 1
+        
 
         #calculate pv (dirty)
         coupons_pv = self.bond.coupon * ((1 - (1+discount_rate)**(-number_periods)) / discount_rate)
-        maturity_pv = self.bond.face_value * ((1+discount_rate)**(- number_periods))
+        maturity_pv = self.bond.face_value * ((1+discount_rate)**(-number_periods))
         dirty_price = coupons_pv + maturity_pv
 
         if self.bond.coupon_frequency == 0: #zero coupon no accrued interest
