1408d70881a861d22fa9cfa55b43953089989cee
wtindall1 <wtindall@hotmail.com>
2023-06-03 22:16:38 +0100

    plan

--- API/Application/ValuationPV.py
+++ API/Application/ValuationPV.py
@@ -11,8 +11,9 @@ class ValuationPV:
     #disount rate can be specified, proxy used if not
     def price(self, discount_rate_specified: Optional[float] = None) -> Optional[Dict[str, float]]:
 
-        #inputs
+        #INPUTS
         coupon_dates = self.bond.get_coupon_dates()
+        
         #for zero coupon bond, set num_periods to number of years
         if self.bond.coupon_frequency == 0:
             number_periods = (coupon_dates[-1] - dt.date.today()).days / 365
@@ -20,12 +21,10 @@ class ValuationPV:
             number_periods = len(coupon_dates)
         
         discount_rate = self.bond.get_proxy_yield() if not discount_rate_specified else discount_rate_specified
-        
         #exit if discount rate not specified and no proxy found
         if discount_rate is None:
             print("No discount rate was specified and no proxy could be found.")
             return None
-        
         #adjust discount rate for coupon frequency
         if self.bond.coupon_frequency > 1:
             discount_rate = (1+discount_rate)**(1/self.bond.coupon_frequency) - 1
@@ -36,6 +35,7 @@ class ValuationPV:
         maturity_pv = self.bond.face_value * ((1+discount_rate)**(-number_periods))
         dirty_price = coupons_pv + maturity_pv
 
+        #calculate clean price
         if self.bond.coupon_frequency == 0: #zero coupon no accrued interest
             clean_price = dirty_price
         else:
